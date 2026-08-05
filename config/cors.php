a8d81fbd1db248b07bd336bf8c755cc6226ae9de
David <ugwuzordavid005@gmail.com>
2026-08-05 12:46:39 +0100

    yehha-cors

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,35 @@
-<?php
+<!-- <?php
+
+// return [
+//     'paths' => ['api/*', 'speed', 'sanctum/csrf-cookie'],
+//     'allowed_methods' => ['*'],
+//     'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+//     'allowed_origins_patterns' => [],
+//     'allowed_headers' => ['*'],
+//     'exposed_headers' => [],
+//     'max_age' => 0,
+//     'supports_credentials' => true,
+// ]; 
 
 return [
     'paths' => ['api/*', 'speed', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
-    'allowed_origins_patterns' => [],
+
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://qora-real.vercel.app',
+    ],
+
+    'allowed_origins_patterns' => [
+        '^https://qora-real-.*\.vercel\.app$',
+    ],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => true,
 ];
